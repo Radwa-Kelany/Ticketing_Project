cf603db57255170a13fa98adbba702f1b8b2f8e7
Radwa Kelany <88267460+Radwa2019@users.noreply.github.com>
2025-04-17 17:46:08 +0300

    remove secret

--- payments/src/stripe.ts
+++ payments/src/stripe.ts
@@ -3,7 +3,6 @@ import Stripe from "stripe";
 const stripe= new Stripe(process.env.STRIPE_KEY!,{
     apiVersion:"2024-12-18.acacia"
 })
-// const stripe = require('stripe')('sk_test_51Qj1n6C6g6sK3D5g1SsjXf6PLNEBVIwN7sP7F6wCmbSb9trErEeSzJRgaHX2R11FPz2CvHVNVmhLeZBKU00YrILR00d2rAkH0n');
 
 
 export default stripe
