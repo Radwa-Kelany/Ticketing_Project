1821c0d30c4f1e4d1ac9fbbc693bd1ef09b114b8
Radwa Kelany <88267460+Radwa2019@users.noreply.github.com>
2025-04-17 17:21:11 +0300

    add client service and docs files

--- expiration/src/events/basics/expiration-complete-event.ts
+++
@@ -1,9 +0,0 @@
-import { Subjects } from './subjects';
-import { OrderStatus } from './orderStatus';
-
-export interface ExpirationCompleteEvent {
-    subject:Subjects.ExpirationComplete
-    data:{
-        id:string
-    }
-}
